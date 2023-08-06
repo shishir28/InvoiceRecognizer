10e8df362525b6f92450b42eed2369645eb940f1
Shishir Mishra <shishir28@gmail.com>
2023-08-06 22:40:11 +1000

    python cosumer written

---
+++ PythonConsumer/invoice.py
@@ -0,0 +1,42 @@
+import os
+from dotenv import load_dotenv
+
+from azure.core.credentials import AzureKeyCredential
+from azure.ai.formrecognizer import DocumentAnalysisClient
+
+import pandas as pd
+
+from tabulate import tabulate
+
+load_dotenv()
+
+endpoint = os.environ['ENV_FORM_RECOGNIZER_ENDPOINT']
+key = os.environ['ENV_FORM_RECOGNIZER_KEY']
+
+model_id = os.environ['ENV_FORM_RECOGNIZER_MODEL_ID']
+formUrl = os.environ['ENV_INPUT_DOCUMENT_URL']
+
+document_analysis_client = DocumentAnalysisClient(
+    endpoint=endpoint, credential=AzureKeyCredential(key)
+)
+
+# Make sure your document's type is included in the list of document types the custom model can analyze
+poller = document_analysis_client.begin_analyze_document_from_url(
+    model_id, formUrl)
+result = poller.result()
+
+for idx, document in enumerate(result.documents):
+    print("--------Analyzing document #{}--------".format(idx + 1))
+    print("Document of '{}' type and '{}' model has confidence {}".format(
+        document.doc_type, result.model_id, document.confidence))
+
+    myDictionary = {'FieldName': 'FieldValue'}
+    for name, field in document.fields.items():
+        field_value = field.value if field.value else field.content
+        myDictionary[name] = field_value
+
+    df = pd.DataFrame(myDictionary.items())
+
+    print(tabulate(df))
+
+print("..........................................")
--- README.md
+++ README.md
@@ -7,17 +7,14 @@ Demo Application which read data from various pdf invoices using custom model of
 ## Tech Stack
 
 - .Net Core
+- Python
 
 ## Architecture Overview
 
 ### Tools
 
-- [Visual Studio 2022](https://visualstudio.microsoft.com/downloads/)
 - [Visual Studio Code](https://code.visualstudio.com/Download/)
-- [Visual Studio for Mac](https://visualstudio.microsoft.com/downloads/)
 - [.Net Core 7 ](https://dotnet.microsoft.com/download/dotnet-core)
-- [Docker Desktop](https://www.docker.com/products/docker-desktop)
-- [Xcode](https://developer.apple.com/xcode/)
 
 ### Housekeeping
 

9af86c67bc97a4cb695859d62ea73b8ddeb1716f
Shishir Mishra <shishir28@gmail.com>
2023-08-08 22:47:03 +1000

    Create cognitive service using az cli

--- az-resources.ps1
+++ az-resources.ps1
@@ -1,9 +1,16 @@
 $resourceGroup = "MonadFormRecoRG"
 $location = "australiaeast"
-$storageAccName = "monadformrecstrg"
-$storageSKU = "Standard_LRS"
+#$storageAccName = "monadformrecstrg"
+#$storageSKU = "Standard_LRS"
+$formRecoName = "MonadFormReco"
+$forRecoSKU = "S0"
 
 # Create resource group
 az group create -n $resourceGroup -l $location
 
-az storage account create  --location $location  --name $storageAccName  --resource-group  $resourceGroup  --sku $storageSKU
+# az storage account create  --location $location  --name $storageAccName  --resource-group  $resourceGroup  --sku $storageSKU
+
+az cognitiveservices account create  --name $formRecoName --resource-group $resourceGroup --kind FormRecognizer --sku $forRecoSKU  --location $location  --yes
+
+
+#az cognitiveservices account show --name "InvoiceFormReco" --resource-group "InvoiceFormRG" --query "properties.endpoint"
